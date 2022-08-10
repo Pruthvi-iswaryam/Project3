0684213f1c1383c894e41115ffb72c40aa40f3d1
Admin <Admin@host.docker.internal>
2022-08-10 11:48:56 +0530

    Adding Controller layer logic

--- NumberToWordConvertor/src/main/java/com/pack/Controller/NumberToWordController.java
+++ NumberToWordConvertor/src/main/java/com/pack/Controller/NumberToWordController.java
@@ -1,5 +1,25 @@
 package com.pack.Controller;
+import com.pack.repository.UnitsAndTensArray;
+import com.pack.service.*;
+	
 
 public class NumberToWordController {
+	
+	private NumberToWordConvertorInterface numbertowordService;
+
+		public NumberToWordController(NumberToWordConvertorInterface numbertowordService) {
+			super();
+			this.numbertowordService = numbertowordService;
+		}
+		
+		public String customNumberInput(int number)
+		{
+			return numbertowordService.isConvertNumbertoWord(number);
+		}
+		
+		
+	}
+
+	
 
 }
