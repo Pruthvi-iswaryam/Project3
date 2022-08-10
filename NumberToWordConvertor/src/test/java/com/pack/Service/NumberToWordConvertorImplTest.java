52960464ca002732fee5dca72afea84153516647
Admin <Admin@host.docker.internal>
2022-08-10 11:42:56 +0530

    Creating test class

---
+++ NumberToWordConvertor/src/test/java/com/pack/Service/NumberToWordConvertorImplTest.java
@@ -0,0 +1,14 @@
+package com.pack.Service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NumberToWordConvertorImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
