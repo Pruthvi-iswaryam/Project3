dcf65f00047c6a79407decfd1aa9ca2a767214bf
Admin <Admin@host.docker.internal>
2022-08-10 11:51:32 +0530

    Adding Repository layer logic

--- NumberToWordConvertor/src/main/java/com/pack/Repository/UnitsAndTensArray.java
+++ NumberToWordConvertor/src/main/java/com/pack/Repository/UnitsAndTensArray.java
@@ -1,5 +1,12 @@
 package com.pack.Repository;
 
-public class UnitsAndTensArray {
+	public class UnitsAndTensArray implements UnitsAndTensArrayInterface
+	{
 
-}
+		public String unitsArray [] = {"","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten",
+			   "Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+
+		
+		
+		public String tensArray [] = {"","","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
+	}
